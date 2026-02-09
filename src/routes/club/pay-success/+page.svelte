9c239fd73aeba3ade9060511480554f6956419fb
Luke Gobin <luke.gobin1@icloud.com>
2026-02-08 20:39:12 -0600

    Create Payment Success Page

---
+++ src/routes/club/pay-success/+page.svelte
@@ -0,0 +1,22 @@
+<div>
+    <h1>Thanks for Your Purchase!</h1>
+    <p>You should receive your role on Discord soon. If you haven't already, please join to receive all of your benefits.</p>
+    <a href="https://discord.gg/ZBwh4YgxRA">
+        <button>JOIN THE DISCORD</button>
+    </a>
+</div>
+
+<style>
+    div {
+        display: flex;
+        flex-direction: column;
+        text-align: center;
+        align-items: center;
+    }
+    div h1 {
+        margin-bottom: 5px;
+    }
+    div a button {
+        min-width: 300px;
+    }
+</style>
